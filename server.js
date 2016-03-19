975e08f119f646c2ef763c9b0047d821f4f0dfff
BranchofLight <scottandrechek@gmail.com>
2016-03-18 23:20:24 -0400

    Initial server code

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 2116;
+
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.method === "GET") {
+    if (req.url === '/currtime') {
+      var time = new Date();
+      time = time.getDate();
+      res.write("THE TIME IS: " + time + '\n');
+    }
+  }
+
+  res.end("Finish.\n");
+}).listen(port, hostname, function() {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
