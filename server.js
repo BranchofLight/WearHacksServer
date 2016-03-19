3c405706417610620f55139253f28eb02c5fd7b2
ploh007@uottawa.ca <ploh007@uottawa.ca>
2016-03-19 00:15:54 -0400

    Added post test

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 var http = require('http');
 
-var hostname = 'localhost';
+var hostname = '192.168.1.100';
 var port = 2116;
+var time = new Date();
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.method === "GET") {
     if (req.url === '/currtime') {
-      var time = new Date();
       time = time.toLocaleString();
       res.write("The time is: " + time + '\n');
     }
   }
 
   if (req.method === "POST") {
+    console.log('POST Req Received');
+
     // var requestBody = '';
-    res.on('data', function(data) {
+    req.on('data', function(data) {
       // requestBody += data;
-      console.log(data);
-      res.write("The time is: " + time + "\n");
+
+      console.log('POST Req2 Received');
+      console.log("Data: " + data);
+      time = time.toLocaleString();
+      res.end("The time is: " + time + "\n");
     });
   }
 
-  res.end("Finish.\n");
+  // res.end("Finish.\n");
 }).listen(port, hostname, function() {
   console.log("Server running at http://" + hostname + ":" + port);
 });
