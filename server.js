aef9534c4851b65a48360a9a8be8037cac29823f
BranchofLight <scottandrechek@gmail.com>
2016-03-19 11:26:37 -0400

    Handled POST request protocols

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var http = require('http');
 
-var hostname = '192.168.1.100';
+var hostname = '192.168.1.10';
 var port = 2116;
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+
   if (req.method === "GET") {
     console.log('GET Request Received');
     if (req.url === '/currtime') {
@@ -16,9 +17,22 @@ http.createServer(function(req, res) {
   if (req.method === "POST") {
     console.log('POST Request Received');
     req.on('data', function(data) {
+      var time = new Date();
       console.log('POST Request Data Received: ' + data);
-      var time = time.toLocaleString();
-      res.end("The time is: " + time + "\n");
+
+      var reqObj = JSON.parse(data);
+      var resObj = {};
+
+      if (reqObj.reqtype === "initpark") {
+        res.end(time.toLocaleString());
+      } else if (reqObj.reqtype === "endpark") {
+        resObj = {
+          "timestamp": time.toLocaleString(),
+          "price": 4.20
+        };
+
+        res.end(resObj);
+      }
     });
   }
 }).listen(port, hostname, function() {
